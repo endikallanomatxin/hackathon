59a98170ee0f75d95a468381e91f1cabf54066ff
Gorka <gorka.ayala@gmail.com>
2025-11-09 16:52:58 +0100

    add lerobot model

--- exercises/genesis/prueba.py
+++ exercises/genesis/prueba.py
@@ -1,11 +1,11 @@
 import genesis as gs
 
-gs.init(backend=gs.cpu)
+gs.init(backend=gs.gpu)
 
 scene = gs.Scene(show_viewer=True)
 plane = scene.add_entity(gs.morphs.Plane())
 franka = scene.add_entity(
-    gs.morphs.MJCF(file='xml/franka_emika_panda/panda.xml'),
+    gs.morphs.MJCF(file='SO101/so101_new_calib.xml'),
 )
 
 scene.build()
