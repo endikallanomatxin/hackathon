504aa4bfa5c8a17e2e1c1991abd79d6ac7e65390
Endika Llano Matxin <endikallano@gmail.com>
2025-11-14 20:42:48 +0100

    restructure exercises

--- exercises/genesis/00_starting_a_simulation_with_lerobot_in_it/main.py
+++ exercises/genesis/00_starting_a_simulation_with_lerobot_in_it/main.py
@@ -1,11 +1,16 @@
+import pathlib
+
 import genesis as gs
 
 gs.init(backend=gs.gpu)
 
 scene = gs.Scene(show_viewer=True)
 plane = scene.add_entity(gs.morphs.Plane())
+
+assets_dir = pathlib.Path(__file__).parent.parent.parent / 'assets'
+robot_file = assets_dir / 'SO101/so101_new_calib.xml'
 franka = scene.add_entity(
-    gs.morphs.MJCF(file='SO101/so101_new_calib.xml'),
+    gs.morphs.MJCF(file=robot_file.as_posix())
 )
 
 scene.build()
